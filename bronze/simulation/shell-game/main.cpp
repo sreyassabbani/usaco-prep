02991e5b90e791a7202ce80a5f845629c7545a1f
sreysus <sreyassabbani@gmail.com>
2025-02-23 11:27:07 -0500

    initial commit: shell game

---
+++ bronze/simulation/shell-game/main.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+using namespace std;
+
+int main() {
+  freopen("shell.in", "r", stdin);
+  freopen("shell.out", "w", stdout);
+
+  // current state of shells
+  vector<int> state = {1, 2, 3};
+   
+  // theoretical score if the pebble was under n-th shell (3 shells)
+  // {0, 0, 0}
+  vector<int> assume(3, 0);
+
+  // number of swaps (number of lines)
+  int S; cin >> S;
+
+  for (int i = 0; i < S; i++) {
+    int a, b, g; cin >> a >> b >> g;
+    
+    // 0-indexed
+    --a;
+    --b;
+    --g;
+
+    // swap
+    int tmp = state[a];
+    state[a] = state[b];
+    state[b] = tmp;
+
+    assume[state[g] - 1]++;
+  }
+
+  int high = 0;
+  for (int i : assume) {
+    high = max(high, i);
+  }
+  cout << high;
+  
+  return 0;
+}
