f4a24d13726941443254108f57f38f30039662c2
sreysus <sreyassabbani@gmail.com>
2025-02-25 20:17:49 -0500

    bronze(simulation): the cow signal, 2016 dec

---
+++ bronze/simulation/cow-signal/main.cpp
@@ -0,0 +1,29 @@
+// 2016 December Contest, Bronze
+// Problem 3. The Cow-Signal 
+
+#include<fstream>
+using namespace std;
+
+int main() {
+  ifstream fin("cowsignal.in");
+  ofstream fout("cowsignal.out");
+  
+  int M, N, K;
+  fin >> M >> N >> K;
+
+  char signal[M * N];
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      fin >> signal[i + j * M];
+    }
+  }
+
+  for (int i = 0; i < M * K; i++) {
+    for (int j = 0; j < N * K; j++) {
+      fout << signal[(i / K) + (j / K) * M];
+    }
+    fout << "\n";
+  }
+
+  return 0;
+}
