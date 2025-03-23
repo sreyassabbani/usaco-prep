7fba7f6cfc2c66bccbf77624e0c10557d0d00aeb
sreysus <sreyassabbani@gmail.com>
2025-03-23 19:06:41 -0400

    bronze(basic complete search): milk pails, feb 2016

---
+++ bronze/simulation/milk-pails/main.cpp
@@ -0,0 +1,31 @@
+// 2016 February Contest, Bronze
+// Problem 1. Milk Pails 
+
+#include <fstream>
+using namespace std;
+
+int main() {
+  ifstream fin("pails.in");
+  ofstream fout("pails.out");
+  
+  int X, Y, M;
+  fin >> X >> Y >> M;
+
+  int closest = min(M - X, M - Y); // represents diff
+
+  int current = X;
+  while (current < M) {
+    int i = 0;
+    while (current <= M - Y) {
+      current += Y;
+      ++i;
+    }
+    closest = min(closest, M - current);
+    current -= Y * i;
+    current += X;
+  }
+
+  fout << M - closest;
+  
+  return 0;
+}
