db61a6428c675ac130b2c79903b2eba21c4e3613
sreysus <sreyassabbani@gmail.com>
2025-07-19 10:50:22 -0400

    bronze(2016 open, complete search, P1): diamond collector

---
+++ bronze/complete-search/diamond-collector/main.cpp
@@ -0,0 +1,40 @@
+// USACO 2016 US Open Contest, Bronze
+// Problem 1. Diamond Collector
+
+#include <algorithm>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ifstream fin("diamond.in");
+    ofstream fout("diamond.out");
+
+    int N, K;
+
+    fin >> N >> K;
+
+    vector<int> diamonds(N);
+
+    for (int i = 0; i < N; ++i) {
+        fin >> diamonds[i];
+    }
+
+    sort(diamonds.begin(), diamonds.end());
+
+    int max_d = 0; // max display-able
+    int cur_d = 0; // cur display-able
+
+    for (int i = 0; i < N; ++i) {
+        cur_d = 0;
+        for (int j = i; j < N; ++j) {
+            if (diamonds[j] - diamonds[i] <= K) {
+                ++cur_d;
+            } else
+                break;
+        }
+        max_d = max(max_d, cur_d);
+    }
+
+    fout << max_d;
+}
